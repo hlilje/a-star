2bd9931f8b586ff50786c13e1da3a392094e8930
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-09-27 21:34:53 +0200

    Add Node struct

---
+++ graph.cpp
@@ -0,0 +1,5 @@
+#include "graph.hpp"
+
+
+Node::Node(const int nX, const int nY) :
+    nX(nX), nY(nY) { }
---
+++ graph.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <vector>
+
+
+struct Node {
+    const int nX, nY;
+
+    Node(const int nX, const int nY);
+};
